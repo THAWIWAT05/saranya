f520822fd067955f68903467df7efbc062898f66
THAWIWAT05 <thawiwat05254905@gmail.com>
2025-02-15 12:47:07 +0700

    Delete src directory

--- src/components/common/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as FloatingHeader } from "./FloatingHeader";
-export { default as FloatingStars } from "./FloatingStars";
-export { default as ImageBox } from "./ImageBox";
-export { default as MemoryZone } from "./MemoryZone";
