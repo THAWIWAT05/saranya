f979ec53bcc061689fd8e9f6329df7810ddca0c5
THAWIWAT05 <thawiwat05254905@gmail.com>
2025-02-15 12:49:06 +0700

    Add files via upload

---
+++ src/assets/mock/mock.js
@@ -0,0 +1,52 @@
+import floating1 from "../images/floating/3.png";
+import floating2 from "../images/floating/4.png";
+import image1 from "../images/1.png";
+import image2 from "../images/2.png";
+import image3 from "../images/3.png";
+import image4 from "../images/4.png";
+import image5 from "../images/5.png";
+import gift1 from "../images/gifts/1.png";
+import gift2 from "../images/gifts/2.png";
+import gift3 from "../images/gifts/3.png";
+import gift4 from "../images/gifts/4.png";
+import star from "../star.svg";
+
+export const _floatingImageFirst = floating1;
+export const _floatingImageSecond = floating2;
+
+export const _albums = [image1, image2, image3, image4, image5];
+
+export const giftAlbums = [gift1, gift2, gift3, gift4];
+
+export const _stars = [
+    {
+        src: star,
+        color: "#ffccd7",
+        size: "12",
+    },
+    {
+        src: star,
+        color: "#ffccd7",
+        size: "4",
+    },
+];
+
+export const _messages = [
+    "วันนี้ตั้งใจทำเว็บไซต์ ไว้เป็นที่ระลึกก่อนจบ ม.6 ให้นะเพื่อน ",
+    "congratulations",
+];
+
+export const _birthdayMessages = [
+    "congratulations",
+    "จบแล้วอย่าลืมทักหากันนะเพื่อน",
+    "อย่าลืมกันนะจบไปแล้วอะ",
+    "ขอบคุณมึงสำหรับทุกอย่างที่เรียนด้วยกัน ที่ให้คำปรึกษา ที่ให้ลอกงาน 55 อันนี้สำคัญ",
+    "ก็จบไปแล้ว ก็เรียนต่อมหาลัยให้มีความสุขนะ เรื่อง ผู้ชาย ก็เบาๆๆนะเพื่อน เรียนให้จบนะ คิดถึงกันก็ทักมา หรือมาดูเว็บนี้ก็ได้เปิดไว้ตลอด@Tiw",
+    "congratulations kub",
+];
+
+// เอาแค่ code ของคลิปมาใส่ หลัง https://www.youtube.com/embed/ เช่น https://youtube.com/shorts/equ0y9yuLYk จะใช้แค่ equ0y9yuLYk
+// https://www.youtube.com/embed/equ0y9yuLYk
+export const _youtubeUrl = "https://www.youtube.com/embed/i2kZbM3coxU?feature=share";
+
+export const _otp = "saranya";
